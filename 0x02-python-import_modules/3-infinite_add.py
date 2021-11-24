47f9a940998521d15ad6db34637ee1943d6e9f24
Faith-goga <83953243+Faith-goga@users.noreply.github.com>
2021-11-24 20:24:01 +0300

    Make changes to the code

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
+    argc = len(sys.argv)
     sum = 0
-    for i in range(1, l):
-        sum += int(argv[i])
-    print('{:d}'.format(sum))
+    for i in range(1, argc):
+        sum += int(sys.argv[i])
+    print(sum)
