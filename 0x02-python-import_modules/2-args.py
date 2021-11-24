626bd8da3e28d33e407871d24927c22fa68b4e96
Faith-goga <83953243+Faith-goga@users.noreply.github.com>
2021-11-24 20:20:22 +0300

    Make all files executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+import sys
+
+
+def main():
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    if l == 1:
-        print('0 argument.')
-    if l == 2:
-        print('1 argument:')
-        print('1: {:s}'.format(argv[1]))
-    if l > 2:
-        print('{:d} arguments:'.format(l-1))
-        for i in range(1, l):
-            print('{:d}: {}'.format(i, argv[i]))
+    main()
